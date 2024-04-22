31e66ef4f8debfcb17a04de2b5024a679c6c178a
Farooqi <rafey-ahmed.farooqi@capgemini.com>
2024-04-22 15:14:52 +0530

    Add pagination to Search Books

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -4,13 +4,15 @@ import "./App.css";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { Navbar } from "./layouts/navbar-and-footers/Navbar";
 import { Footer } from "./layouts/navbar-and-footers/Footer";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 
 
 export const App=()=> {
   return (
     <div>
       <Navbar/>
-      <HomePage/>
+      {/* <HomePage/> */}
+      <SearchBooksPage/>
       <Footer/>
     </div>
     
